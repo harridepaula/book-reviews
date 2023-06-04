9e39178f36df5eb0efa7369cb64991943302bcb4
Harri <harri.kinsman@gmail.com>
2023-06-04 21:16:14 +0100

    Update Home.js

--- book-reviews/my-app/src/Components/Home/Home.js
+++ book-reviews/my-app/src/Components/Home/Home.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import BookCard from './BookCard';
 
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    // Fetch search results when the search term changes
+    // Replace this with your actual API call or data source logic
+    const fetchSearchResults = async () => {
+      // Mocking data for demonstration purposes
+      const response = await fetch(`https://api.example.com/books?search=${searchTerm}`);
+      const data = await response.json();
+      setSearchResults(data);
+    };
+
+    fetchSearchResults();
+  }, [searchTerm]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -10,6 +25,16 @@ const Home = () => {
   return (
     <div>
       <input type="text" value={searchTerm} onChange={handleSearch} placeholder="Search for books..." />
+      <div className="search-results">
+        {searchResults.map((book) => (
+          <BookCard
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            coverImage={book.coverImage}
+          />
+        ))}
+      </div>
     </div>
   );
 };
